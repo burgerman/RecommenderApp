d365a0e716456809d1b1e36358abae925ebe0a83
yashahuja1 <yashrahuja311@gmail.com>
2024-07-27 12:18:01 -0400

    update with Global Item(java object)

---
+++ app/src/androidTest/java/com/group7/recommenderapp/RecommenderServiceTestMusic.java
@@ -0,0 +1,148 @@
+package com.group7.recommenderapp;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.test.InstrumentationRegistry;
+
+import com.group7.recommenderapp.entities.ContentItem;
+import com.group7.recommenderapp.entities.MusicItem;
+import com.group7.recommenderapp.service.MusicRecommender;
+import com.group7.recommenderapp.service.MusicConfig;
+import com.group7.recommenderapp.service.RecommenderService;
+import com.group7.recommenderapp.util.FileUtilMusic;
+
+import org.junit.Before;
+import org.junit.Test;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RecommenderServiceTestMusic {
+//    @Mock
+//    private static Context mockContext;
+//    @Mock
+//    private static Resources mockResources;
+
+    private static final String CONFIG_PATH = "music_config.json";  // Default config path in assets.
+    private static final String TAG = "RecommenderServiceUnitTest";
+    private static MusicConfig config;
+    private static RecommenderService musicRecommender;
+    private static final int RANDOM_BOUNDS = 10;
+    private Context ctx;
+    private final Random rand = new Random();
+
+    @Before
+    public void setUp() {
+        ctx = InstrumentationRegistry.getTargetContext();
+
+        try {
+            config = FileUtilMusic.loadMovieConfig(ctx.getAssets(), CONFIG_PATH);
+        } catch (IOException ex) {
+            Log.e(TAG, String.format("Error occurs when loading config %s: %s.", CONFIG_PATH, ex));
+        }
+        musicRecommender = MusicRecommender.getInstance(ctx, config);
+        musicRecommender.load();
+//        musicRecommender = MusicBasedRecommender.getInstance(ctx);
+//        musicRecommender.loadModel();
+
+    }
+
+    @Test
+    public void musicRecommendByGenreTest() throws IOException {
+        // get genre list from assets file
+        List<String> genres =  FileUtilMusic.loadGenreList(ctx.getAssets(), config.genreList);
+
+        // randomly select some genres from asset file
+        Collections.shuffle(genres);
+        List<String> selectedGenres = genres.subList(0, rand.nextInt(RANDOM_BOUNDS)+1);
+        Log.i(TAG, "selectedGenres1: " + selectedGenres);
+
+        // assert the recommendation list is non-empty and correct in terms of length, rating/score and genre
+        List<ContentItem> recommendedMusic1 = musicRecommender.recommendByGenre(selectedGenres);
+        Log.i(TAG, "recommendedMusics1: " + recommendedMusic1);
+
+
+        assertTrue("The recommendation list is not empty", recommendedMusic1.size() > 0);
+        float lastScore = 10000;
+        float newScore = 0;
+        for (ContentItem item : recommendedMusic1) {
+//            assertTrue("genres of recommendedMusics is contained in user's selection", genres.contains(item.getGenres()));
+            newScore = item.getScore();
+
+            assertTrue("score of recommendedMusic is in decreasing order", lastScore >= newScore);
+            lastScore = newScore;
+        }
+
+        // randomly select another few genres
+        Collections.shuffle(genres);
+        selectedGenres = genres.subList(0, rand.nextInt(RANDOM_BOUNDS)+1);
+        Log.i(TAG, "selectedGenres2: " + selectedGenres);
+
+        // assert the recommendation list is different from previous one
+        List<ContentItem> recommendedMusic2 = musicRecommender.recommendByGenre(selectedGenres);
+        Log.i(TAG, "recommendedMusic2: " + recommendedMusic1);
+
+        List<ContentItem> differences12 = new ArrayList<>(recommendedMusic1);
+        differences12.removeAll(recommendedMusic2);
+
+        List<ContentItem> differences21 = new ArrayList<>(recommendedMusic2);
+        differences21.removeAll(recommendedMusic1);
+
+        assertTrue("there's at least one difference between two recommendation lists", !differences12.isEmpty() || !differences21.isEmpty());
+    }
+
+    @Test
+    public void musicRecommendByItemTest() throws IOException {
+        // get music list from assets file
+        List<MusicItem> music = (List<MusicItem>) FileUtilMusic.loadMusicList(ctx.getAssets(), config.musicList);
+
+        // randomly select some genres from asset file
+        Collections.shuffle(music);
+        List<MusicItem> selectedMusic = music.subList(0, rand.nextInt(RANDOM_BOUNDS)+1);
+        Log.i(TAG, "selectedMusic1: " + selectedMusic);
+
+        // assert the recommendation list is non-empty and correct in terms of length, rating/score and genre
+        List<MusicItem> recommendedMusic1 = musicRecommender.recommendByItem(selectedMusic);
+        Log.i(TAG, "recommendedMusic1: " + recommendedMusic1);
+
+        assertTrue("The recommendation list is not empty", !recommendedMusic1.isEmpty());
+        float lastConfidence = 1;
+        float newConfidence = 0;
+        for (MusicItem item : recommendedMusic1) {
+            newConfidence = item.getConfidence();
+
+            assertTrue("confidence of recommendedMusic is in decreasing order", lastConfidence >= newConfidence);
+            lastConfidence = newConfidence;
+        }
+
+        // randomly select another few items
+        Collections.shuffle(music);
+        selectedMusic = music.subList(0, rand.nextInt(RANDOM_BOUNDS)+1);
+        Log.i(TAG, "selectedMusic2: " + selectedMusic);
+
+        // assert the recommendation list is different from previous one
+        List recommendedMusic2 = musicRecommender.recommendByItem(selectedMusic);
+        Log.i(TAG, "recommendedMusic2: " + recommendedMusic1);
+
+        List<MusicItem> differences12 = new ArrayList<>(recommendedMusic1);
+        differences12.removeAll(recommendedMusic2);
+
+        List<MusicItem> differences21 = new ArrayList<>(recommendedMusic2);
+        differences21.removeAll(recommendedMusic1);
+
+        assertTrue("there's at least one difference between two recommendation lists", !differences12.isEmpty() || !differences21.isEmpty() );
+    }
+}
---
+++ app/src/main/java/com/group7/recommenderapp/service/MusicConfig.java
@@ -0,0 +1,100 @@
+package com.group7.recommenderapp.service;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Config for music recommendation app. */
+public final class MusicConfig {
+    private static final String TAG = "MusicConfig";
+    private static final String DEFAULT_MODEL_PATH = "music_recommendation_model.tflite";
+    private static final String DEFAULT_MUSIC_LIST_PATH = "sorted_music_vocab.json";
+    private static final String DEFAULT_GENRE_LIST_PATH = "music_genre_vocab.txt";
+    private static final int DEFAULT_OUTPUT_LENGTH = 100;
+    private static final int DEFAULT_TOP_K = 10;
+    private static final int PAD_ID = 0;
+    private static final int UNKNOWN_GENRE = 0;
+    private static final int DEFAULT_OUTPUT_IDS_INDEX = 0;
+    private static final int DEFAULT_OUTPUT_SCORES_INDEX = 1;
+    private static final int DEFAULT_FAVORITE_LIST_SIZE = 100;
+
+    public static final String FEATURE_MUSIC = "musicFeature";
+    public static final String FEATURE_GENRE = "genreFeature";
+    public static final String FEATURE_RATING = "ratingFeature";
+
+    /** Feature of the model. */
+    public static class Feature {
+        /** Input feature name. */
+        public String name;
+        /** Input feature index. */
+        public int index;
+        /** Input feature length. */
+        public int inputLength;
+    }
+
+    /** TF Lite model path. */
+    public String model = DEFAULT_MODEL_PATH;
+
+    /** List of input features */
+    public List<Feature> inputs = new ArrayList<>();
+    /** Number of output length from the model. */
+    public int outputLength = DEFAULT_OUTPUT_LENGTH;
+    /** Number of max results to show in the UI. */
+    public int topK = DEFAULT_TOP_K;
+    /** Path to the music list. */
+    public String musicList = DEFAULT_MUSIC_LIST_PATH;
+    /** Path to the genre list. Use genre feature if it is not null. */
+    public String genreList = DEFAULT_GENRE_LIST_PATH;
+
+    /** Id for padding. */
+    public int pad = PAD_ID;
+
+    /** Music genre for unknown. */
+    public int unknownGenre = UNKNOWN_GENRE;
+
+    /** Output index for ID. */
+    public int outputIdsIndex = DEFAULT_OUTPUT_IDS_INDEX;
+    /** Output index for score. */
+    public int outputScoresIndex = DEFAULT_OUTPUT_SCORES_INDEX;
+
+    /** The number of favorite music tracks for users to choose from. */
+    public int favoriteListSize = DEFAULT_FAVORITE_LIST_SIZE;
+
+    public MusicConfig() {}
+
+    public boolean validate() {
+        if (inputs.isEmpty()) {
+            Log.e(TAG, "config inputs should not be empty");
+            return false;
+        }
+
+        boolean hasGenreFeature = false;
+        for (MusicConfig.Feature feature : inputs) {
+            if (FEATURE_GENRE.equals(feature.name)) {
+                hasGenreFeature = true;
+                break;
+            }
+        }
+        if (useGenres() || hasGenreFeature) {
+            if (!useGenres() || !hasGenreFeature) {
+                String msg =
+                        "If uses genre, must set both `genreFeature` in inputs and `genreList` as vocab.";
+                if (!useGenres()) {
+                    msg += "`genreList` is missing.";
+                }
+                if (!hasGenreFeature) {
+                    msg += "`genreFeature` is missing.";
+                }
+                Log.e(TAG, msg);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean useGenres() {
+        return genreList != null;
+    }
+}
