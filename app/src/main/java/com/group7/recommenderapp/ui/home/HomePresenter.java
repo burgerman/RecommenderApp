0e33cabadfe19b58b9128e4469d59a2dd26ea5f0
Sayan <sayan.2000@hotmail.com>
2024-08-04 20:46:29 -0400

    HomePresenter Updated

--- app/src/main/java/com/group7/recommenderapp/ui/home/HomePresenter.java
+++ app/src/main/java/com/group7/recommenderapp/ui/home/HomePresenter.java
@@ -36,85 +36,27 @@ public class HomePresenter implements HomeContract.Presenter {
     @Override
     public void loadMovieContent() {
         // Load movie recommendations
+        movieRecommenderService.load();
         UserService us = UserService.getUserServiceInstance(context);
         UserProfile up = us.selectUserProfile(us.getCurrentUser());
         List<String> genres = (List<String>) up.getPreferences().getPreferenceDict().get("categoriesClass1");
         List<ContentItem> recommendedMovies = movieRecommenderService.recommendByGenre(genres!=null?genres: Arrays.asList("Action", "Comedy"));
-//        ContentItem item1 = new ContentItem();
-//        item1.setScore(5);
-//        item1.setGenres(Arrays.asList("Comedy", "Action"));
-//        item1.setTitle("movie 1");
-//        item1.setId(11);
-//        item1.setConfidence(90.0f);
-//        item1.setSelected(false);
-//        ContentItem item2 = new ContentItem();
-//        item1.setScore(4);
-//        item1.setGenres(Arrays.asList("Comedy", "Action"));
-//        item1.setTitle("movie 2");
-//        item1.setId(18);
-//        item1.setConfidence(96.0f);
-//        item1.setSelected(false);
-//        ContentItem item3 = new ContentItem();
-//        item1.setScore(3);
-//        item1.setGenres(Arrays.asList("Comedy", "Action"));
-//        item1.setTitle("movie 3");
-//        item1.setId(12);
-//        item1.setConfidence(80.0f);
-//        item1.setSelected(false);
-//        ContentItem item4 = new ContentItem();
-//        item1.setScore(2);
-//        item1.setGenres(Arrays.asList("Comedy", "Action"));
-//        item1.setTitle("movie 4");
-//        item1.setId(28);
-//        item1.setConfidence(70.0f);
-//        item1.setSelected(false);
-//        ResultSet movieResultSet = new ResultSet();
-//        movieResultSet.setItemset(Arrays.asList(item1, item2,item3,item4));
         ResultSet movieResultSet = new ResultSet();
         movieResultSet.setItemset(recommendedMovies);
-        view.showRecommendedMovieContent(movieResultSet.getTopK(4));
+        view.showRecommendedMovieContent(movieResultSet.getTopK(10));
     }
 
     @Override
     public void loadMusicContent() {
         // Load music recommendations
+        musicRecommenderService.load();
         UserService us = UserService.getUserServiceInstance(context);
         UserProfile up = us.selectUserProfile(us.getCurrentUser());
         List<String> genres = (List<String>) up.getPreferences().getPreferenceDict().get("categoriesClass2");
         List<ContentItem> recommendedMusic = musicRecommenderService.recommendByGenre(genres!=null?genres: Arrays.asList("Rock", "Pop"));
-//        ContentItem item1 = new ContentItem();
-//        item1.setScore(5);
-//        item1.setGenres(Arrays.asList("Pop", "Rock"));
-//        item1.setTitle("music 1");
-//        item1.setId(21);
-//        item1.setConfidence(90.0f);
-//        item1.setSelected(false);
-//        ContentItem item2 = new ContentItem();
-//        item1.setScore(4);
-//        item1.setGenres(Arrays.asList("Pop", "Rock"));
-//        item1.setTitle("music 2");
-//        item1.setId(19);
-//        item1.setConfidence(96.0f);
-//        item1.setSelected(false);
-//        ContentItem item3 = new ContentItem();
-//        item1.setScore(3);
-//        item1.setGenres(Arrays.asList("Pop", "Rock"));
-//        item1.setTitle("music 3");
-//        item1.setId(40);
-//        item1.setConfidence(80.0f);
-//        item1.setSelected(false);
-//        ContentItem item4 = new ContentItem();
-//        item1.setScore(2);
-//        item1.setGenres(Arrays.asList("Pop", "Rock"));
-//        item1.setTitle("music 4");
-//        item1.setId(30);
-//        item1.setConfidence(70.0f);
-//        item1.setSelected(false);
-//        ResultSet musicResultSet = new ResultSet();
-//        musicResultSet.setItemset(Arrays.asList(item1, item2, item3, item4));
         ResultSet musicResultSet = new ResultSet();
         musicResultSet.setItemset(recommendedMusic);
-        view.showRecommendedMusicContent(musicResultSet.getTopK(4));
+        view.showRecommendedMusicContent(musicResultSet.getTopK(10));
     }
 
     @Override
